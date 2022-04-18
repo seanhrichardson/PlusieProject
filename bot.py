e61e7ef4d8ae3949b79df600d841caf653c2745b
Sean Richardson <seanhrichardson@gmail.com>
2022-04-18 16:20:54 -0700

    setup.py

--- bot.py
+++ bot.py
@@ -17,6 +17,36 @@ b = 0.75 #probability of heads if weighted
 A0 = 0.5 #initial probability of fair
 B0 = 0.5 #intial probability of weighted
 
+#COORDINATES:
+# top left of heads-tails:
+ht1x, ht1y = 916, 412
+# bottom right of heads-tails:
+ht2x, ht2y = 1092, 504
+#1 flip
+flip1x, flip1y = 831, 887
+#fair
+fairx, fairy = 798, 1034
+#cheater
+cheatx, cheaty = 1110, 1024
+#5 flips
+flip5x, flip5y = 1104, 888
+#top of flips left
+fl1x, fl1y = 1059, 801
+#bottom flips left
+fl2x, fl2y = 1255, 839
+#name
+namex, namey = 0,0
+#email
+emailx, emaily = 0,0
+#submit
+submitx, submity = 0,0
+#reset
+resetx, resety = 0,0
+
+name_for_leaderboard = "sean"
+email_for_plushie = "seanrichardson98@gmail.com"
+
+
 def prob_fair(n,k):
     binom = math.comb(n,k)
     return (binom*a**k*(1-a)**(n-k)*A0)/(A0*binom*a**k*(1-a)**(n-k)+B0*binom*b**k*(1-b)**(n-k))
@@ -27,7 +57,7 @@ def type_name():
     time.sleep(.5)
     pyautogui.click()
     time.sleep(.5)
-    pyautogui.typewrite("sean")
+    pyautogui.typewrite(name_for_leaderboard)
 
 def type_email():
     time.sleep(.5)
@@ -35,7 +65,7 @@ def type_email():
     time.sleep(.5)
     pyautogui.click()
     time.sleep(.5)
-    pyautogui.typewrite("seanrichardson98@gmail.com")
+    pyautogui.typewrite(email_for_plushie)
 
 def click_submit():
     time.sleep(.5)
@@ -50,32 +80,36 @@ def click_reset():
     pyautogui.click()
 
 def flip5():
-    pyautogui.moveTo(1200, 900)
+    # pyautogui.moveTo(1200, 900)
+    pyautogui.moveTo(flip5x, flip5y)
     pyautogui.click()
 
 def flip():
-    pyautogui.moveTo(800, 900)
+    # pyautogui.moveTo(800, 900)
+    pyautogui.moveTo(flip1x, flip1y)
     pyautogui.click()
 
 def label_as_cheater():
-    pyautogui.moveTo(1200, 1000)
+    # pyautogui.moveTo(1200, 1000)
+    pyautogui.moveTo(cheatx, cheaty)
     pyautogui.click()
     time.sleep(2)
 
 def label_as_fair():
-    pyautogui.moveTo(800, 1000)
+    pyautogui.moveTo(fairx, fairy)
     pyautogui.click()
     time.sleep(2)
 
 def text_to_heads_tails(image_text):
-    if image_text == "":
-        return 0,0
     result = re.findall(r'\d+', image_text)
+    if len(result) < 2:
+        return 0,0
     return int(result[0]), int(result[1])
 
 def screenshot_heads_tails():
-    image = ImageGrab.grab(bbox=(920, 425, 1100, 500))
-    image = ImageOps.invert(image)
+    # image = ImageGrab.grab(bbox=(920, 425, 1100, 500))
+    image = ImageGrab.grab(bbox=(ht1x, ht1y, ht2x, ht2y))
+    image = numpy.invert(image)
     #image.save("temp/image.png")
     #image = Image.open('temp/image.png')
     image_to_text = pytesseract.image_to_string(image, lang='eng')
@@ -105,7 +139,8 @@ def bad_engine(heads,tails):
         flip5()
 
 def get_flips_left():
-    image = ImageGrab.grab(bbox=(1060, 800, 1200, 850))
+    # image = ImageGrab.grab(bbox=(1060, 800, 1200, 850))
+    image = ImageGrab.grab(bbox=(fl1x, fl1y, fl2x, fl2y))
     image = numpy.invert(image)
     save_name = "temp/num_flips.png"
     # image.save(save_name)
@@ -124,7 +159,7 @@ def game_is_over():
 
 def game_is_over2():
     image = ImageGrab.grab(bbox=(700, 500, 1200, 600))
-    image = ImageOps.invert(im)
+    image = numpy.invert(im)
     # save_name = "temp/game_over_check.png"
     # image.save(save_name)
     # image = Image.open(save_name)
---
+++ setup.py
@@ -0,0 +1,75 @@
+import pyautogui
+
+things = [
+["top left of heads-tails:", "ht1"],
+["bottom right of heads-tails", "ht2"],
+["1 flip", "flip1"],
+["guess fair", "fair"],
+["guess cheater", "cheat"],
+["5 flips", "flip5"],
+["top of flips left", "fl1"],
+["bottom of flips left", "fl2"],
+["name for leaderboard coordinates", "name"],
+["email for plushie drawing coordinates", "email"],
+["submit coords", "submit"],
+["reset coords", "reset"]
+]
+
+ret = ""
+for field in things:
+    input(field[0])
+    pos = pyautogui.position()
+    varx, vary = str(pos[0]), str(pos[1])
+    ret += field[1] + "x" + "," + field[1] + "y" + "=" + varx + "," + vary + "\n"
+
+print("\nCOPY AND PASE THE BELOW INTO BOT CODE")
+print(ret)
+# str1 = "top left of heads-tails:"
+# input(str1)
+# ret = str1 + "\t" + str(pyautogui.position()) + "\n"
+
+# str2 = "bottom right of heads-tails:"
+# input(str2)
+# ret += str2 + "\t" + str(pyautogui.position()) + "\n"
+
+# str3 = "1 flip\t\t\t"
+# input(str3)
+# ret += str3 + "\t" + str(pyautogui.position()) + "\n"
+
+# str4 = "guess fair\t\t"
+# input(str4)
+# ret += str4 + "\t" + str(pyautogui.position()) + "\n"
+
+# str5 = "guess cheater\t\t"
+# input(str5)
+# ret += str5 + "\t" + str(pyautogui.position()) + "\n"
+
+# str6 = "5 flips\t\t\t"
+# input(str6)
+# ret += str6 + "\t" + str(pyautogui.position()) + "\n"
+
+# str7 = "top of flips left\t"
+# input(str7)
+# ret += str7 + "\t" + str(pyautogui.position()) + "\n"
+
+# str8 = "bottom of flips left\t"
+# input(str8)
+# ret += str8 + "\t" + str(pyautogui.position()) + "\n"
+
+# str9 = "name for leaderboard coordinates"
+# input(str9)
+# ret += str9 + "\t" + str(pyautogui.position()) + "\n"
+
+# str10 = "email for plushie drawing coordinates"
+# input(str10)
+# ret += str10 + "\t" + str(pyautogui.position()) + "\n"
+
+# str11 = "submit coordinates"
+# input(str11)
+# ret += str11 + "\t" + str(pyautogui.position()) + "\n"
+
+# str12 = "reset game"
+# input(str12)
+# ret += str12 + "\t" + str(pyautogui.position()) + "\n"
+
+# print(ret)
